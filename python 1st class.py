c6721d75ae23b2d881a32bbb99495eb42ca4a6ac
mangaraiarchana <mangaraiarchana16@gmail.com>
2025-12-09 04:23:55 +0000

    Created using Colab

---
+++ python 1st class.py
@@ -0,0 +1,85 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOX2sIOxRbGyNe07NPPSdls",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/mangaraiarchana/demo-github/blob/main/python%201st%20class.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 11,
+      "metadata": {
+        "id": "9-sw5cuBRsBR",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "outputId": "b0990ac9-b833-43d9-fb91-93461313fce0"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "30\n"
+          ]
+        }
+      ],
+      "source": [
+        "# area of rectangle\n",
+        "L =10\n",
+        "B =3\n",
+        "a = L * B\n",
+        "print(a)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "x = int(input(\"enter length value \"))\n",
+        "y = int(input(\"enter breadth value \"))\n",
+        "a = x * y\n",
+        "print(a)"
+      ],
+      "metadata": {
+        "id": "6jDAYPWRTMAG",
+        "outputId": "0c665a7f-320e-4e9e-95b6-f89cb5c86f77",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        }
+      },
+      "execution_count": 19,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "enter length value 2\n",
+            "enter breadth value 3\n",
+            "6\n"
+          ]
+        }
+      ]
+    }
+  ]
+}
